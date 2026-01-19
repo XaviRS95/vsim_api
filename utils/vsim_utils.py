d33c76588fa0fff91fb0bca8c478154929d0b312
XaviRS95 <draculcodes@gmail.com>
2026-01-19 19:21:29 +0100

    Full implementation with vlog complete

--- utils/vsim_utils.py
+++ utils/vsim_utils.py
@@ -1,52 +1,36 @@
-import subprocess
-import re
+import os, re, subprocess
 from models.models import SyntaxResponse
 
 def syntax_checker(code:str):
-
     filename = "prueba.sv"
+    try:
+        with open(filename, "w", encoding="utf-8") as f:
+            f.write(code)
 
-    with open(filename, "w", encoding="utf-8") as f:
-        f.write(code)
-
-    result = subprocess.run(
-        ["vlog",filename],
-        capture_output=True,
-        text=True
-    )
-
-    if result.returncode == 0:
-        return SyntaxResponse(
-            error = 0,
+        result = subprocess.run(
+            ["vlog", filename],
+            capture_output=True,
+            text=True
         )
-    else
 
-        return result.stdout
+        if result.returncode == 0:
+            return SyntaxResponse(
+                result="OK"
+            )
+        else:
+            errors = error_trace_extractor(trace=result.stdout)
+            return SyntaxResponse(
+                result=errors
+            )
+    except Exception as e:
+        print(e)
+    finally:
+        # Clean up auxiliary file
+        if os.path.exists(filename):
+            os.remove(filename)
 
-def error_trace_extractor(trace: str)-> list:
-
+def error_trace_extractor(trace: str)-> str:
     pattern = r"(\*\* Error:.*(?:\n\*\* Error:.*)*)"
-
     matches = re.findall(pattern, trace)
-
-    for match in matches:
-        print("--- Error Trace ---")
-        print(match)
-
-    return matches
-
-code = """
-module case_example1(input logic [1:0] a, output logic y);
-    always_comb begin
-        case(a)
-            2'b00: y = 0;
-            2'b01: y = 1;
-            2'b10: y = 1;
-            2'b11: y = 0;
-            default: y = 0;
-        endcase
-    end
-endmodue
-"""
-
-print(syntax_checker(code=code))
+    combined = "\n".join(matches)
+    return combined
